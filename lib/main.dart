d595ff64507921376326135166de58c0a4277b55
Lalit Chandnani <lalit.chandnani@urbanladder.com>
2023-04-16 20:14:23 +0530

    fixed layout and text alignment issues with the code

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,9 @@ class _DietFormState extends State<DietForm> {
   final _ageController = TextEditingController();
   final _weightController = TextEditingController();
   final _heightController = TextEditingController();
-  Map<String, dynamic> _data = {};
+  Map<String, dynamic> _recommendationContent = {};
   bool _isLoading = false;
+  bool _displayRecommendation = true;
 
   Future<void> _submitForm() async {
     setState(() {
@@ -50,15 +51,24 @@ class _DietFormState extends State<DietForm> {
 
       Api api = Api();
       final data = await api.fetchDietRecommendations(name, age, weight, height);
+      
+      // Reset the form
+      _formKey.currentState?.reset();
+      _nameController.clear();
+      _ageController.clear();
+      _weightController.clear();
+      _heightController.clear();
 
       setState(() {
         _isLoading = false; // Set isLoading to false to hide the loading icon
-        _data = data;
+        _displayRecommendation = true;
+        _recommendationContent = data;
       });
     } else {
       setState(() {
         _isLoading = false; // Set isLoading to false to hide the loading icon
-        _data = {}; // Clear the data if form validation fails
+        _displayRecommendation = false;
+        _recommendationContent = {}; // Clear the data if form validation fails
       });
     }
   }
@@ -74,124 +84,154 @@ class _DietFormState extends State<DietForm> {
 
   @override
   Widget build(BuildContext context) {
-    final nameValidator = RegExp(r"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
-    final ageValidator = RegExp(r"^(?:1[01][0-9]|100|[6-9][0-9]|[1-9][0-9]|[5-9])$");
-    final weightValidator = RegExp(r"^(?:1[0-4][0-9]|150|[6-9][0-9]|[1-9][0-9]|[5-9])$");
-    final heightValidator = RegExp(r"^(?:2[01][0-5]|1\d\d|1[3-9][0-9]|[5-9]\d|1[0-2][0-4])$");
-
-    return MaterialApp(
-      title: 'AI Diet Planner',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text('Diet Planner'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: _isLoading
-              ? Center(child: CircularProgressIndicator()) // Show the loading icon if isLoading is true
-              : Form(
-                  key: _formKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        'Enter your details:',
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    if (_displayRecommendation && _recommendationContent.isNotEmpty){
+      return MaterialApp(
+        title: 'AI Diet Planner',
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            leading: IconButton(  
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                setState(() {
+                  _displayRecommendation = false;
+                });
+              },
+            ),
+            title: Text('Recommendation'),
+          ),
+          body: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  height: MediaQuery.of(context).size.height * 0.9,
+                  child: Center(child: SingleChildScrollView(
+                      child: Text(
+                        _recommendationContent['recommendation'],
+                        style: TextStyle(fontSize: 20.0),
                       ),
-                      TextFormField(
-                        controller: _nameController,
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          labelText: 'Name',
-                          hintText: 'Enter your name',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your name';
-                          } else if (!nameValidator.hasMatch(value)) {
-                            return 'Please enter a valid name';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: _ageController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          labelText: 'Age',
-                          hintText: 'Enter your age',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your age';
-                          } else if (!ageValidator.hasMatch(value)) {
-                            return 'Please enter a valid age between 5 and 100';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: _weightController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          labelText: 'Weight (kg)',
-                          hintText: 'Enter your weight',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your weight';
-                          } else if (!weightValidator.hasMatch(value)) {
-                            return 'Please enter a valid weight between 5 and 150 kg';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: _heightController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          labelText: 'Height (cm)',
-                          hintText: 'Enter your height',
-                          border: OutlineInputBorder(),
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your height';
-                          } else if (!heightValidator.hasMatch(value)) {
-                            return 'Please enter a valid height between 125 and 215 cm';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 16.0),
-                      Center(
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
-                          ),
-                          onPressed: _submitForm,
-                          child: Text('Submit'),
-                        ),
-                      ),
-                      SizedBox(height: 16.0),
-                      if (_data.isNotEmpty)
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Recommendation:'),
-                            Text('${_data['recommendation']}'),
-                          ],
-                        ),
-                    ],
+                    ),
                   ),
                 ),
+      )
+     );
+    } else {
+      final nameValidator = RegExp(r"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+      final ageValidator = RegExp(r"^(?:1[01][0-9]|100|[6-9][0-9]|[1-9][0-9]|[5-9])$");
+      final weightValidator = RegExp(r"^(?:1[0-4][0-9]|150|[6-9][0-9]|[1-9][0-9]|[5-9])$");
+      final heightValidator = RegExp(r"^(?:2[01][0-5]|1\d\d|1[3-9][0-9]|[5-9]\d|1[0-2][0-4])$");
+
+      return MaterialApp(
+        title: 'AI Diet Planner',
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: Text('Diet Planner'),
+            leading: Image.asset('images/ai_diet_planner2.png'),
+          ),
+          body: 
+          Container(
+            height: MediaQuery.of(context).size.height * 0.9,
+            padding: const EdgeInsets.all(16.0),
+            child: _isLoading
+                ? Center(child: CircularProgressIndicator()) // Show the loading icon if isLoading is true
+                : Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Center(child: Text(
+                            'Enter your details',
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        TextFormField(
+                          controller: _nameController,
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                            hintText: 'Enter your name',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your name';
+                            } else if (!nameValidator.hasMatch(value)) {
+                              return 'Please enter a valid name';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 8.0),
+                        TextFormField(
+                          controller: _ageController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            labelText: 'Age',
+                            hintText: 'Enter your age',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your age';
+                            } else if (!ageValidator.hasMatch(value)) {
+                              return 'Please enter a valid age between 5 and 100';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 8.0),
+                        TextFormField(
+                          controller: _weightController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            labelText: 'Weight (kg)',
+                            hintText: 'Enter your weight',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your weight';
+                            } else if (!weightValidator.hasMatch(value)) {
+                              return 'Please enter a valid weight between 5 and 150 kg';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 8.0),
+                        TextFormField(
+                          controller: _heightController,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            labelText: 'Height (cm)',
+                            hintText: 'Enter your height',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your height';
+                            } else if (!heightValidator.hasMatch(value)) {
+                              return 'Please enter a valid height between 125 and 215 cm';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 16.0),
+                        Center(
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
+                            ),
+                            onPressed: _submitForm,
+                            child: Text('Submit'),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
